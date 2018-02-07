a9b9813c2a78dc63130d4f78a7ed68ad92e7e354
liron maliker <kaskasof1@gmail.com>
2018-02-07 15:04:19 +0200

    Add files via upload

---
+++ browser-based Ski activity counter/src/components/containers/GoalProgress.js
@@ -0,0 +1,25 @@
+import GoalProgress from '../ui/GoalProgress'
+import { connect } from 'react-redux'
+import { setGoal } from '../../actions'
+
+const mapStateToProps = state =>
+	({
+		current: state.allSkiDays.length,
+		goal: state.goal
+	})
+
+const mapDispatchToProps = dispatch =>
+	({
+		onNewGoal(goal) {
+			dispatch(
+				setGoal(goal)
+			)
+		}
+	})
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalProgress)
+
+// export default () =>
+//     <GoalProgress current={10}
+//                   goal={20}
+//                   onNewGoal={goal => console.log('todo: change goal', goal)} />
